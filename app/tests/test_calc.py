30e209d0dd87c19d2a16216c645138546841d1d1
Aunim HASSAN <aunim.hassan@gmail.com>
2025-08-16 13:30:32 +0200

    test: cd down

--- app/tests/test_calc.py
+++ app/tests/test_calc.py
@@ -2,7 +2,7 @@ from utils.calc import addition, substraction
 
 
 def test_addition():
-    assert addition(5, 5) == 10
+    assert addition(5, 5) == 8
 
 def test_substraction():
     assert substraction(5, 5) == 0
